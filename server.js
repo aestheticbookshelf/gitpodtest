698fac5b9104db253adde7dc1f979bdfccf4f908
aestheticbookshelf <aestheticbookshelf@gmail.com>
2020-10-15 15:10:53 +0000

    Static

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path")
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
@@ -36,6 +37,10 @@ if (process.env.NODE_ENV !== "production") {
 
 app.get('/', (req, res) => {
   res.send(require("./server/welcome.js").welcomeMessage())
+})
+
+app.get('/static', (req, res) => {
+  res.sendFile(path.join(__dirname, "server/static.html"))
 })
 
 app.listen(port, () => {
