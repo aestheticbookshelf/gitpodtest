c10fd1cb8c46558733ee023b31ea439a44b92f05
aestheticbookshelf <aestheticbookshelf@gmail.com>
2020-10-15 13:47:42 +0000

    Browser sync

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3000
 
 if (process.env.NODE_ENV !== "production") {
   const chokidar = require("chokidar");
