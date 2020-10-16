d3f9a1928ff2b4165bef5e0a225c5c85fb624978
aestheticbookshelf <aestheticbookshelf@gmail.com>
2020-10-16 07:56:39 +0000

    Streamline

--- server/server.js
+++ server/server.js
@@ -4,12 +4,14 @@ const app = express()
 const port = process.env.PORT || 3000
 
 const stamp = new Date().getTime()
+let lastStamp = null
 
 app.get('/stamp', (req, res) => {
+    lastStamp = new Date().getTime()
     res.send(`${stamp}`)
 })
 
-let welcomeMessage = "Welcome !!"
+let welcomeMessage = "Welcome !"
 
 app.get('/', (req, res) => {
     res.send(`
@@ -18,21 +20,18 @@ app.get('/', (req, res) => {
             <title>Reload Express Sample App</title>
         </head>
         <body>
-            <h1>${welcomeMessage}</h1>
-            <!-- All you have to do is include the reload script and have it be on every page of your project -->
-            <!-- You do not create this route, reload creates it for you automatically -->    
+            <h1>${welcomeMessage}</h1>            
             <script>
-            let stamp = null
-            setInterval(_=>{
-                fetch('/stamp').then(response=>response.text().then(content=>{
-                    console.log(content)
-                    if(stamp){
-                        if(content != stamp) setTimeout(_=>document.location.reload(), 200)
-                    }else{
-                        stamp = content
-                    }
-                }))
-            }, 200)    
+                let stamp = null
+                setInterval(_=>{
+                    fetch('/stamp').then(response=>response.text().then(content=>{                        
+                        if(stamp){
+                            if(content != stamp) setTimeout(_=>document.location.reload(), 500)
+                        }else{
+                            stamp = content
+                        }
+                    }))
+                }, 200)    
             </script>
         </body>
     </html>
@@ -42,13 +41,20 @@ app.get('/', (req, res) => {
 let devPublic
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`app listening at http://localhost:${port}`)
 
-  devPublic = `http://localhost:${port}`
+  devPublic = `http://localhost:${port}/`
 
   try{
-    devPublic = require('child_process').execSync(`gp url ${port}`).toString().trim()
+    devPublic = require('child_process').execSync(`gp url ${port}`).toString().trim()+"/"
   }catch(err){}
   
   console.log("dev public : " + devPublic)
+
+  setInterval(_=>{
+      if((!lastStamp) || (new Date().getTime()-lastStamp>1000)){
+          console.log("opening browser")
+          require('open')(devPublic)
+      }
+  }, 2000)
 })
