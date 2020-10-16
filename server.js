3ac309c5680446fa2ac00183c50ef79275b5019c
aestheticbookshelf <aestheticbookshelf@gmail.com>
2020-10-16 02:01:12 +0000

    Reload

--- server.js
+++ server.js
@@ -4,7 +4,20 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send(require("./server/welcome.js").welcomeMessage())
+    res.send(`
+    <!doctype html>
+<html>
+  <head>
+    <title>Reload Express Sample App</title>
+  </head>
+  <body>
+    <h1>${require('./server/welcome.js').welcomeMessage()}</h1>
+    <!-- All you have to do is include the reload script and have it be on every page of your project -->
+    <!-- You do not create this route, reload creates it for you automatically -->
+    <script src="/reload/reload.js"></script> 
+  </body>
+</html>
+    `)  
 })
 
 app.get('/static', (req, res) => {
